805cf40e01f386654b71e47bed01fa5f3d91208d
leterbry <leterbry@sct-16-acfcl.ur.univ-rouen.fr>
2021-10-07 11:14:51 +0200

    origin bryce

--- server.R
+++ server.R
@@ -1,64 +1,59 @@
-library(shiny)
-library(plotly)
-library(shinydashboard)
-library(htmlwidgets)
+#
+# This is the server logic of a Shiny web application. You can run the
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
 
-function(input, output) {
-        # When the analysis is started
-        observeEvent(input$start, {
-                # Get data from the uploaded file
-                data <- reactive({
-                        read.csv(file = input$file$datapath, sep = ";", header = TRUE)
-                })
-                # Create the action when the curser hovers on the plot
-                addHoverBehavior <- "function(el, x){
-                        el.on('plotly_hover', function(data){
-                                var infotext = data.points.map(function(d){
-                                        console.log(d)
-                                        return (d.data.name[d.pointNumber]+': x= '+d.x+', y= '+d.y.toPrecision(3));
-                                });
-                                console.log(infotext)
-                                Shiny.onInputChange('hover_data', infotext)
-                        })
-                }"
-                renderPlotly2 <- function (expr, env = parent.frame(), quoted = FALSE){
-                        if (!quoted) {
-                                expr <- substitute(expr)
-                        }
-                        shinyRenderWidget(expr, plotlyOutput, env, quoted = TRUE)
-                }
-                # Generate the volcano plot
-                output$volcano <- renderPlotly2({
-                        x <- data()$log2FC
-                        y <- data()$pval
-                        d <- data.frame(x,y)
-                        print(d$y)
-                        d$genes <- data()$GeneName
-                        d$diffexpressed <- "NO"
-                        d$diffexpressed[d$x > input$FC & d$y < input$pvalue] <- "UP"
-                        d$diffexpressed[d$x < -input$FC & d$y < input$pvalue] <- "DOWN"
-                        mycolors <- c("blue", "red", "black")
-                        names(mycolors) <- c("DOWN", "UP", "NO")
-                        p <- plot_ly(data = d, x = x, y = -log2(y), text = d$genes, mode = "markers", color = d$diffexpressed, colors = mycolors)
-                        as_widget(p) %>% onRender(addHoverBehavior)
-                })
-                # Print genes name when the curser hovers on the points
-                output$hover <- renderText({
-                        input$hover_data
-                })
-                output$MA <- renderPlotly2({
-                        x <- data()$baseMean
-                        y <- data()$pval
-                        d <- data.frame(x,y)
-                        print(d$y)
-                        d$genes <- data()$GeneName
-                        d$diffexpressed <- "NO"
-                        d$diffexpressed[d$x > input$FC & d$y < input$pvalue] <- "UP"
-                        d$diffexpressed[d$x < -input$FC & d$y < input$pvalue] <- "DOWN"
-                        mycolors <- c("blue", "red", "black")
-                        names(mycolors) <- c("DOWN", "UP", "NO")
-                        p <- plot_ly(data = d, x = -log2(y), y = x, text = d$genes, mode = "markers", color = d$diffexpressed, colors = mycolors)
-                        as_widget(p) %>% onRender(addHoverBehavior)
-                })
+library(shiny)
+library(ggplot2)
+
+server <- function(input, output){
+    
+    observeEvent(input$go, {
+
+        # Data input managment
+        data <- reactive({
+            req(input$file)
+            df <- read.csv(input$file$datapath, sep = ";")
+            df["log2padj"] <- -log2(df["padj"])
+            df
         })
+        
+        # Data view
+        output$table <- DT::renderDataTable({
+            data()
+        })
+        
+        # Volcano plot managment
+        volcanoPlot <- reactive({
+            df <- data()
+            
+            df["DEG"] <- ifelse(df["log2FC"] >= input$foldChangeIn & df["padj"] <= input$pvalueIn, "DEG+", 
+                                ifelse(df["log2FC"] <= -input$foldChangeIn & df["padj"] <= input$pvalueIn,"DEG-",
+                                       "Non regulated")
+            )
+            
+            View(df)
+            
+            ggplot(data = df, aes(x=log2FC, y=log2padj, col=DEG)) + geom_point()
+        })
+        
+        # Volcano plot view
+        output$volcano <- renderPlot({
+            volcanoPlot()
+        }) 
+        
+        # Volcano plot download
+        output$downloadVolcano <- downloadHandler(
+            filename = "volcano.png",
+            content = function(file) {
+                ggsave(file,volcanoPlot())
+            }
+        )
+    })
 }
+
+
--- ui.R
+++ ui.R
@@ -1,89 +1,116 @@
-library(shinydashboard)
+## M2.1 BIMS / Promo 2021-2023
+## LETERRIR Bryce
+## Universite de Rouen
 
-dashboardPage(
-        skin = "black",
-        dashboardHeader(title = "Application"),
-                # Sidebar content
-                dashboardSidebar(
-                        sidebarMenu(
-                                # File selection
-                                fileInput("file", label = "File input"),
-                                # verbatimTextOutput("file"),
-                                # Select box : organism selection
-                                selectInput("select_organism", label = "Organism of interest",
-                                        choices = list("Homo Sapiens" = 1, "Mus musculus" = 2, "Arabidopsis Thaliana" = 3),
-                                        selected = 1
-                                ),
-                                # verbatimTextOutput("select_organism"),
-                                # Start Button
-                                actionButton("start", label = "Start analysis"),
-                                hr(),
-                                # Menu 
-                                menuItem("Whole Data Inspection", tabName = "whole_data_inspection", icon = icon("database")),
-                                menuItem("GO Term Enrichment", tabName = "GO_enrichment", icon = icon("sitemap")),
-                                menuItem("Pathway Enrichment", tabName = "path_enrichment", icon = icon("chart-pie")),
-                                menuItem("Protein Domain Enrichment", tabName = "prot_enrichment", icon = icon("th")),
-                                menuItem("About", tabName = "about", icon = icon("th"))
-                        )
-                ),
-        # Body content
-        dashboardBody(
-                tabItems(
-                        # First tab content : Whole data inspection
-                        tabItem(tabName = "whole_data_inspection",
-                                # Box vo
-                                box(
-                                        title = "Volcano plot",
-                                        solidHeader = TRUE,
-                                        status = "primary",
-                                        plotlyOutput("volcano")
-                                ),
-                                box(
-                                        title = "OPTIONS",
-                                        solidHeader = TRUE,
-                                        status = "primary",
-                                        sliderInput(
-                                                inputId = "pvalue", 
-                                                label = "p-value cutoff from input :",
-                                                min = 0, 
-                                                max = 1,
-                                                value = 0.05),
-                                        sliderInput(
-                                                inputId = "FC", 
-                                                label = "log2 Fold-Change cutoff from input :", 
-                                                min = 0, 
-                                                max = 5, 
-                                                value = 1),
-                                        downloadButton(
-                                                outputId = "download_volcano", 
-                                                label = "Download volcano plot", 
-                                        ),
-                                        downloadButton(
-                                                outputId = "download_MA", 
-                                                label = "Download MA plot", 
-                                        )
-                                ),
-                                box(
-                                        title = "MA plot",
-                                        solidHeader = TRUE,
-                                        status = "primary",
-                                        plotlyOutput("MA")
-                                ),
-                                box(
-                                        title = "SET UP FOR SUBSEQUENT ANALYSES",
-                                        solidHeader = TRUE,
-                                        status = "primary",
-                                        sliderInput(
-                                                inputId = "FC", 
-                                                label = "p-value cutoff for subsequent analyses :", 
-                                                min = 0, 
-                                                max = 1, 
-                                                value = 0.05)
-                                )
-                        ),
-                        # Second tab content : GO Terms Enrichment
-                        tabItem(tabName = "GO_enrichment"
-                        )
-                )
+library(shiny)
+library(shinydashboard)
+# library(DT)
+
+# Define UI for application that draws a histogram
+ui <- dashboardPage(
+    dashboardHeader(
+        title = "Enrichissement fonctionnel",
+        titleWidth = 500
+    ),
+    
+    ## ---
+    ## SIDEBAR
+    ## ---
+    
+    dashboardSidebar(            
+        fileInput("file", label = h2("File input")),
+        selectInput(inputId = "organism",label = "Organism",choices = c("Arabidopsis thaliana", "Homo sapiens")),
+        
+        actionButton(inputId = "go",label = "Run Application"),
+        sidebarMenu(
+            menuItem("Whole Data Inspection",tabName = "WDInspection"),
+            menuItem("GO Term Enrichment",tabName = "GOEnrich"),
+            menuItem("Pathway Enrichment",tabName = "PathEnrich"),
+            menuItem("Protein Domain Enrichment",tabName = "PDEnrich"),
+            menuItem("About",tabName = "About")
         )
+    ),
+    
+    ## ---
+    ## BODY
+    ## ---
+    
+    dashboardBody(
+        tabItems(
+            tabItem(tabName = "WDInspection",
+                    
+                    # Box valcano plot
+                    box(status = "warning", solidHeader = TRUE, collapsible = FALSE, title = "Volcano Plot",
+                        plotOutput(outputId = "volcano"),
+                    ),
+                    
+                    # Box input options
+                    box(status = "warning", solidHeader = TRUE, collapsible = FALSE, title = "Options",
+                        sliderInput(inputId = "pvalueIn",
+                                    label = "p-value cutoff from input", 
+                                    value = 0.05, 
+                                    min = 0, max = 1, 
+                                    step = 0.01
+                        ),
+                        
+                        sliderInput(inputId = "foldChangeIn",label = "log2 FoldChange cutoff from input", 
+                                    value = 1, 
+                                    min = 0, max = 5, 
+                                    step = 0.5
+                        ), 
+                        
+                        downloadButton(outputId = "downloadVolcano",
+                                     label = "Download Volcano Plot", 
+                                     icon = icon("fas fa-download")
+                        ), 
+                        
+                        actionButton(inputId = "downloadMA",
+                                     label = "Download MA Plot", 
+                                     icon = icon("fas fa-download")
+                        )
+                    ),
+                    
+                    # Box MA plot
+                    box(status = "warning", solidHeader = TRUE, collapsible = FALSE, title = "MA Plot",
+                        
+                    ),
+                    
+                    # Box output options
+                    box(status = "warning", solidHeader = TRUE, collapsible = FALSE, title = "Set up for subsequent analyses",
+                        sliderInput(inputId = "pvalueOut",
+                                    label = "p-value cutoff for subsequent analyses", 
+                                    value = 0.05, 
+                                    min = 0, max = 1, 
+                                    step = 0.01,)
+                    ),
+                    
+                    DT::dataTableOutput(outputId = "table")
+            ),
+            
+            tabItem(tabName = "GOEnrich",
+                    box(status = "warning", solidHeader = TRUE, collapsible = FALSE, title = "Volcano plot",width = 12,
+                        plotOutput(outputId = "show4")
+                    )
+            ),
+            
+            tabItem(tabName = "PathEnrich",
+                    # Box colnames
+                    box(status = "warning", solidHeader = TRUE, collapsible = FALSE, title = "Column names",
+                        verbatimTextOutput(outputId = "colNames"),
+                        verbatimTextOutput(outputId = "selectedOrganism")
+                    ),
+            ),
+            
+            tabItem(tabName = "PDEnrich",
+                   
+            ),
+            tabItem(tabName = "About",
+                    
+            )
+        )
+    )
 )
+
+
+
+
