14fcb31bd6519c8c7f69a36bf6fe206ca20c069b
meijemathe <76946555+meijemathe@users.noreply.github.com>
2021-11-12 12:28:06 +0100

    Add files via upload

--- server.R
+++ server.R
@@ -1,4 +1,3 @@
-spety
 # Projet d'annotation fonctionnel avec R Shiny 
 
 # Auteurs :
@@ -22,12 +21,7 @@ if (!require('plotly', quietly = T)) install.packages('plotly');
 if (!require('htmlwidgets', quietly = T)) install.packages('htmlwidgets');
 if (!require('shinyalert', quietly = T)) install.packages('shinyalert');
 if (!require('shinyjs', quietly = T)) install.packages('shinyjs');
-#if (!require('BiocManager', quietly = T)) install.packages('BiocManager');
 
-
-# BiocManager::install("biomaRt")
-# BiocManager::install("clusterProfiler")
-# BiocManager::install("pathview")
 library(shiny)
 library(shinydashboard)
 library(DT)
@@ -40,133 +34,27 @@ library(shinyjs)
 # Debut
 function(input, output) {
         observeEvent(input$file, {
-          req(input$file)
-          #test extension
-          ext <- tools::file_ext(input$file$datapath)
-          validate(need(ext == "csv", "Please upload a csv file"))
-          # Vector with good columns names
-          good <-c("GeneName", "ID", "baseMean", "log2FC", "pval", "padj")
-          # Get data from the uploaded file
-          df <- read.csv(input$file$datapath, sep = ";")
-          #Check data format, colnames and GeneID
-          if(ncol(df)!=6)
-          {
-            shinyalert("Column Error","Uploaded Data has the wrong number of columns\n Need 6 columns : GeneName, ID, baseMean, log2FC, pval, padj",
-                       type="error")
-            returnValue()
-          }
-
-          else if(identical(good,colnames(df))==FALSE)
-          {
-            shinyalert("Column names Error",paste0("Wrong format for column ",which(colnames(df) != good),", it must be : ",good[which(colnames(df) != good)],"\n",collapse=""),type = "error")
-            returnValue()
-          }
-        })
-        # When the analysis is started
-        observeEvent(input$start, {
                 req(input$file)
+                #test extension
+                ext <- tools::file_ext(input$file$datapath)
+                validate(need(ext == "csv", "Please upload a csv file"))
+                # Vector with good columns names
+                good <-c("GeneName", "ID", "baseMean", "log2FC", "pval", "padj")
                 # Get data from the uploaded file
-                data <- reactive({
-                        req(input$file)
-                        df <- read.csv(input$file$datapath, sep = ";")
-                        df["log2padj"] <- -log2(df["padj"])
-                        df
-                })
-                # Data view
-                output$table <- DT::renderDataTable({
-                        data()
-                })
-                # Create the action when the curser hovers on the plot
-                addHoverBehavior <- "function(el, x){
-                        el.on('plotly_hover', function(data){
-                                var infotext = data.points.map(function(d){
-                                        console.log(d)
-                                        return (d.data.name[d.pointNumber]+': x= '+d.x+', y= '+d.y.toPrecision(3));
-                                });
-                                console.log(infotext)
-                                Shiny.onInputChange('hover_data', infotext)
-                        })
-                }"
-                renderPlotly2 <- function (expr, env = parent.frame(), quoted = FALSE){
-                        if (!quoted) {
-                                expr <- substitute(expr)
-                        }
-                        shinyRenderWidget(expr, plotlyOutput, env, quoted = TRUE)
+                df <- read.csv(input$file$datapath, sep = ";")
+                #Check data format, colnames and GeneID
+                if(ncol(df)!=6)
+                {
+                        shinyalert("Column Error","Uploaded Data has the wrong number of columns\n Need 6 columns : GeneName, ID, baseMean, log2FC, pval, padj",
+                        type="error")
+                        returnValue()
                 }
-                # Generate the volcano plot
-                volcanoPlot <- reactive({
-                        df <- data()
-                        df["DEG"] <- ifelse(df["log2FC"] >= input$FC & df["padj"] <= input$pvalue, "UP", 
-                                        ifelse(df["log2FC"] <= -input$FC & df["padj"] <= input$pvalue,"DOWN",
-                                        "NO")
-                                )
-                        mycolors <- c("blue", "orange", "grey")
-                        names(mycolors) <- c("DOWN", "UP", "NO")
-                        plot_ly(data = df, x = df$log2FC, y = df$log2padj, text = df$GeneName, mode = "markers", color = df$DEG, colors = mycolors)
-                        
-                })
-                output$volcano <- renderPlotly2({
-                        p <- volcanoPlot()
-                        as_widget(p) %>% 
-                          onRender(addHoverBehavior) %>%
-                          config(modeBarButtons = list(list("zoomIn2d"), list("zoomOut2d"), list("select2d"), list("resetScale2d"), list("toImage")))
-                        
-                })
-                # Print genes name when the curser hovers on the points
-                output$hover <- renderText({
-                        input$hover_data
-                })
-                MAPlot <- reactive({
-                        df <- data()
-                        df["DEG"] <- ifelse(df["log2FC"] >= input$FC & df["padj"] <= input$pvalue, "UP", 
-                                            ifelse(df["log2FC"] <= -input$FC & df["padj"] <= input$pvalue,"DOWN",
-                                                   "NO")
-                        )
-                        mycolors <- c("blue", "orange", "grey")
-                        names(mycolors) <- c("DOWN", "UP", "NO")
-                        plot_ly(data = df, x = log2(df$baseMean), y = df$log2FC, text = df$GeneName, mode = "markers", color = df$DEG, colors = mycolors) 
-                        
-                })
-                output$MA <- renderPlotly2({
-                        p <- MAPlot()
-                        as_widget(p) %>% 
-                          onRender(addHoverBehavior) %>% 
-                          config(modeBarButtons = list(list("zoomIn2d"), list("zoomOut2d"), list("select2d"), list("resetScale2d"), list("toImage")))
-                })
+                else if(identical(good,colnames(df))==FALSE)
+                {
+                        shinyalert("Column names Error",paste0("Wrong format for column ",which(colnames(df) != good),", it must be : ",good[which(colnames(df) != good)],"\n",collapse=""),type = "error")
+                        returnValue()
+                } 
         })
-}
-
-# Projet d'annotation fonctionnel avec R Shiny 
-
-# Auteurs :
-
-# MATHE Meije : meije.mathe@univ-rouen.fr
-# PETY Solene : solene.pety@etu.univ-rouen.fr
-# LETERRIER Bryce : bryce.leterrier@univ-rouen.fr
-# OLLIVIER Louis : louis.ollivier@etu.univ-rouen.fr
-
-# M2.1 BIMS - Univ. Rouen Normandie 
-
-# 2021 - 2022
-
-#####################################################################
-# Initialisation des packages 
-
-if (!require('shiny', quietly = T)) install.packages('shiny');
-if (!require('shinydashboard', quietly = T)) install.packages('shinydashboard');
-if (!require('DT', quietly = T)) install.packages('DT');
-if (!require('plotly', quietly = T)) install.packages('plotly');
-if (!require('htmlwidgets', quietly = T)) install.packages('htmlwidgets');
-
-library(shiny)
-library(shinydashboard)
-library(DT)
-library(plotly)
-library(htmlwidgets)
-#####################################################################
-
-# Debut
-function(input, output) {
         # When the analysis is started
         observeEvent(input$start, {
                 req(input$file)
@@ -210,7 +98,10 @@ function(input, output) {
                                 )
                         mycolors <- c("blue", "orange", "grey")
                         names(mycolors) <- c("DOWN", "UP", "NO")
-                        plot_ly(data = df, x = df$log2FC, y = df$log2padj, text = df$GeneName, mode = "markers", color = df$DEG, colors = mycolors)
+                        plot_ly(data = df, x = df$log2FC, y = df$log2padj, text = df$GeneName, mode = "markers", color = df$DEG, colors = mycolors) %>%
+                          layout(xaxis = list(title = 'log2(FC)'),
+                                 yaxis = list(title = 'log2(padj)'),
+                                 title = list(text = paste("Volcano Plot ", input$select_organism), x = 0))
                         
                 })
                 output$volcano <- renderPlotly2({
@@ -230,7 +121,10 @@ function(input, output) {
                         )
                         mycolors <- c("blue", "orange", "grey")
                         names(mycolors) <- c("DOWN", "UP", "NO")
-                        plot_ly(data = df, x = log2(df$baseMean), y = df$log2FC, text = df$GeneName, mode = "markers", color = df$DEG, colors = mycolors)
+                        plot_ly(data = df, x = log2(df$baseMean), y = df$log2FC, text = df$GeneName, mode = "markers", color = df$DEG, colors = mycolors) %>%
+                                layout(xaxis = list(title = "log2(baseMean)"),
+                                       yaxis = list(title = "log2(FC)"),
+                                       title = list(text = paste("MA Plot ", input$select_organism), x = 0))
                         
                 })
                 output$MA <- renderPlotly2({
@@ -239,4 +133,3 @@ function(input, output) {
                 })
         })
 }
-main
