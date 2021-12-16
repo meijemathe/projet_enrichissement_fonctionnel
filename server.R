551c16db1ab18ca819520d1ddb116c9f345d0d6d
bleterrier <76951594+bleterrier@users.noreply.github.com>
2021-12-16 16:29:50 +0100

    scatter type in plots

--- server.R
+++ server.R
@@ -178,7 +178,7 @@ function(input, output) {
                                 )
                         mycolors <- c("blue", "orange", "grey")
                         names(mycolors) <- c("DOWN", "UP", "NO")
-                        plot_ly(data = df, x = df$log2FC, y = df$log2padj, text = df$GeneName, mode = "markers", color = df$DEG, colors = mycolors) %>%
+                        plot_ly(data = df, x = df$log2FC, y = df$log2padj, text = df$GeneName, mode = "markers", type = "scatter", color = df$DEG, colors = mycolors) %>%
                           layout(xaxis = list(title = 'log2(FC)'),
                                  yaxis = list(title = 'log2(padj)'),
                                  title = list(text = paste("Volcano Plot ", input$select_organism), x = 0))
@@ -241,7 +241,7 @@ function(input, output) {
                         )
                         mycolors <- c("blue", "orange", "grey")
                         names(mycolors) <- c("DOWN", "UP", "NO")
-                        plot_ly(data = df, x = log2(df$baseMean), y = df$log2FC, text = df$GeneName, mode = "markers", color = df$DEG, colors = mycolors) %>%
+                        plot_ly(data = df, x = log2(df$baseMean), y = df$log2FC, text = df$GeneName, mode = "markers", type = "scatter", color = df$DEG, colors = mycolors) %>%
                                 layout(xaxis = list(title = "log2(baseMean)"),
                                        yaxis = list(title = "log2(FC)"),
                                        title = list(text = paste("MA Plot ", input$select_organism), x = 0))
